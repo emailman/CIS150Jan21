d113145dba8016fccaf8ac084faba7518982a855
emailman <emailman@dtcc.edu>
2016-01-21 10:25:26 -0500

    First released version

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -5,7 +5,7 @@ package p1;
  * Demo program for CIS 150
  * Eric Mailman
  * 21Jan16
- * Version 0.1
+ * Version 1.0
  */
 
 import java.util.Scanner;
@@ -17,6 +17,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         char option;
+        double price;
 
         System.out.println("Welcome to Jimmy Johns\nWhat would you like?");
         do {
@@ -26,12 +27,51 @@ public class Main {
             System.out.println("(S)alami");
             System.out.println("(T)una");
             System.out.println("(D)one");
+            System.out.print("Enter your choice: ");
 
-            // Get the user input
+            // Get the user input and convert to upper case
             String choice = input.next();
+            choice = choice.toUpperCase();
+
             // Store the first letter in a char
             option = choice.charAt(0);
 
+            // Switch based on the option
+            switch (option) {
+                // Ham
+                case 'H':
+                    price = 6.50;
+                    break;
+
+                // Roast beef
+                case 'R':
+                    price = 6.75;
+                    break;
+
+                // Salami
+                case 'S':
+                    price = 6.25;
+                    break;
+
+                // Tuna
+                case 'T':
+                    price = 5.50;
+                    break;
+
+                // Done
+                case 'D':
+                    price = 0;
+                    break;
+
+                default:
+                    System.out.println("Option " + choice + " is not available");
+                    price = 0;
+            }
+
+            if (price != 0) {
+                System.out.println("you chose: " + choice);
+                System.out.printf("cost: $%.2f\n", price);
+            }
 
         } while (option != 'D');
         System.out.println("Thank you for choosing Jimmy Johns");
